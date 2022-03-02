a8718b5769e55482495a7a71a26fb8225df206c2
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2022-03-02 12:22:07 +0900

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,13 @@
+def add(*args):
+    result=0
+    for i in args:
+        result+=i
+    return result
+
+def mul(*args):
+    result=1
+    for i in args:
+        result*=i
+    return result
+
+    
