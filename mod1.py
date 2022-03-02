f79d285b9065828732d6de5f9a3056aa4bc01f08
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2022-03-02 14:20:37 +0900

    mod1

---
+++ mod1.py
@@ -0,0 +1,12 @@
+def add(*args):
+    result=0
+    for i in args:
+        result+=i
+    return result
+
+def mul(*args):
+    result=1
+    for i in args:
+        result*=i
+    return result
+   
